3a5330c32ff7c24835cb1643d4a7e3d6cd229dde
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-04-18 06:38:12 +0200

    updated

--- edu_django_backend/core/api/urls.py
+++ edu_django_backend/core/api/urls.py
@@ -1,6 +1,9 @@
 
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path,include
 
 urlpatterns = [
-    
+    path('token/',MyTokenObtainPairView.as_view(), name = 'token' ),
+    path('token/refresh/',TokenRefreshView.as_view(), name = 'token-refresh' ),
 ]
