4760019ed5e7b48fd5b67a6c6fc875c753ad82de
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-06-30 07:47:03 +0300

    models created

--- edu_django_backend/core/models.py
+++ edu_django_backend/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-
+import uuid
 
 class UserManager(BaseUserManager):
 
@@ -38,9 +38,19 @@ class User(AbstractUser):
     """
     Custom user model that supports using email instead of username
     """
+    ROLE_CHOICES = [
+        ('admin', 'Administrator'),
+        ('instructor', 'Instructor'),
+        ('student', 'Student'),
+        ('content_creator', 'Content Creator'),
+        ('parent', 'Parent/Guardian'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = None
     email = models.EmailField(_("email address"), unique=True)
     phone_number = models.CharField(verbose_name=_("phone number"),max_length=20,unique= True, help_text='Enter phone number')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    role_assigned_date = models.DateField(default=timezone.now)
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = ['email']
@@ -49,4 +59,77 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+class ContentCreator(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
+class Instructor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL)
+    instructor_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Course(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    course_name = models.CharField(max_length=100)
+    description = models.TextField()
+    instructor_id = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Lesson(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(auto_now_add=True)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_text = models.CharField(max_length=255)
+    options = models.CharField(max_length=500)
+    correct_answer = models.CharField(max_length=100)
+    difficulty_level = models.CharField(max_length=50)
+    lesson_id = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    content_creator_id = models.ForeignKey(ContentCreator, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class QuestionOption(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=100)
+
+class Exam(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    exam_date = models.DateField()
+    exam_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class ExamQuestion(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    exam_id = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam_id = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Feedback(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    difficulty_level = models.CharField(max_length=50)
+
+@receiver(post_save, sender=User)
+def create_student(sender, instance, created, **kwargs):
+    if created and instance.role == 'student':
+        Student.objects.create(user=instance)
